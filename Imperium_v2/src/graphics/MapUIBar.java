9b770d9bf3df715cac4b2542b225e1bf3975c856
Peter Wyld <Peter Wyld@DESKTOP-30F45M5>
2017-12-12 10:04:15 +0000

    Adding MapEditorUI interactivity

--- Imperium_v2/src/graphics/MapUIBar.java
+++ Imperium_v2/src/graphics/MapUIBar.java
@@ -35,13 +35,25 @@ public class MapUIBar extends JPanel{
 		return cycle;
 	}
 	
+	public void lftBtnClick() {
+		if (cycle > 0) {
+			cycle -= 1;
+		}
+	}
+	
+	public void rghtBtnClick() {
+		if ((Math.ceil(imgArr.length -1)/9.0) < cycle) {
+			cycle += 1;
+		}
 	}
 
 	public void paintComponent(Graphics g) {
 		Graphics g2d = (Graphics2D) g;
 		g2d.drawImage(bgImage, 0, 0, MainValues.width, MainValues.width/4, null);
+		g2d.drawImage(lftBtn, MainValues.resolution, MainValues.resolution/5, MainValues.resolution/2, MainValues.resolution/2, null);
+		g2d.drawImage(rghtBtn, 11 * MainValues.resolution, MainValues.resolution/5, MainValues.resolution/2, MainValues.resolution/2, null);
 		for (int i = 0 ; i <= 8; i++) {
-			g2d.drawImage(imgArr[i+cycle], (i*2+4) * 5 * MainValues.resolution/10, 2 * MainValues.resolution/10, 5 * MainValues.resolution/10, 5 * MainValues.resolution/10, null);
+			g2d.drawImage(imgArr[i+cycle], (i*2+4) * MainValues.resolution/2, MainValues.resolution/5, MainValues.resolution/2, MainValues.resolution/2, null);
 			if (cycle + i >= 15) {
 				cycle = 0;
 			}
